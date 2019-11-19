0970a15111e3e29165a745b907e43cb514fc5d28
BroNaz <brodnikovnv@gmail.com>
2019-11-19 13:20:25 +0300

    add RecyclerView

--- app/src/main/java/com/example/password/view/HomeFragment.kt
+++ app/src/main/java/com/example/password/view/HomeFragment.kt
@@ -3,8 +3,15 @@ package com.example.password.view
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.password.R
+import com.example.password.adapter.MyAdapter
 
 class HomeFragment : Fragment() {
     override fun onAttach(context: Context) {
@@ -12,8 +19,26 @@ class HomeFragment : Fragment() {
         Log.d("LOL", "homedOnAttach")
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("LOL", "homeOnViewCreated")
+        val myDataset: Array<String> = Array(11) { "$it" }
+        val viewManager = LinearLayoutManager(context)
+        val viewAdapter = MyAdapter(myDataset)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        recyclerView.adapter = viewAdapter
+
+
+        Toast.makeText(context, viewAdapter.itemCount.toString(), Toast.LENGTH_LONG).show()
+
     }
 }
